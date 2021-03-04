dbd78b8e6d0c5a5e340a4f508c98149f10b42620
Lucki Alan Fernandez Salcedo <alan.lucki@gmail.com>
2021-03-03 19:12:56 -0500

    varios

--- src/routes/r_index.js
+++ src/routes/r_index.js
@@ -2,16 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/v_usuario_omn', async (req, res) => {
-
-    res.render('v_usuario_omn', null);
-
+  res.render('v_usuario_omn', null);
 });
 
 router.get('*', async (req, res) => {
-
-    res.render('v_index', null);
-
+  res.render('v_index', null);
 });
-
 
 module.exports = router;
