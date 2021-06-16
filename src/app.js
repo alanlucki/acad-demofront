32b45e092d0170d91e4295c9a6093558dc40d3d1
Lucki Alan Fernandez Salcedo <alan.lucki@gmail.com>
2021-06-16 03:04:09 -0500

    varios

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/dx', express.static(__dirname + '/public/dx'));
-app.use('/image', express.static(__dirname + '/public/image'));
+app.use('/images', express.static(__dirname + '/public/images'));
 
 
 // routes
@@ -32,7 +32,7 @@ app.set('view engine', 'ejs');
 
 
 // starting the server
-app.listen(3001, () => {
+app.listen(8888, () => {
 
     console.log('Server on port 3001');
 
